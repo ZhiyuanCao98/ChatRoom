e435ec3b458a16f5a03c5376fd6e83001a1f8e08
Cvanzy <cao.zhi@husky.neu.edu>
2018-11-14 19:48:14 -0500

    add teimstaple

--- public/js/index.js
+++ public/js/index.js
@@ -13,18 +13,22 @@ socket.on('disconnect', function() {
 
 
 socket.on('newMessage', function(m) {
+  var formattedTime = moment(m.createdAt).format('h:mm a');
   console.log('newMessage', m );
   var li = jQuery('<li></li>');
-  li.text(`${m.from}: ${m.text}`);
+  li.text(`${m.from} ${formattedTime}: ${m.text}`);
 
   jQuery('#messages').append(li);
 });
 
 socket.on('newLocationMessage', function(message){
+  var formattedTime = moment(message.createdAt).format('h:mm a');
+  console.log('newMessage', m );
+
   var li = jQuery('<li></li>');
   var a = jQuery('<a target="_blank">My current location</a>');
 
-  li.text(`${message.from}:`);
+  li.text(`${message.from}: ${formattedTime}`);
   a.attr('href', message.url);
   li.append(a);
   jQuery('#messages').append(li);
