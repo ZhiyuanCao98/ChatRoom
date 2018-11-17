e0a4a891b3338c673ee76aca84a00aca87985867
Cvanzy <cao.zhi@husky.neu.edu>
2018-11-17 14:56:21 -0500

    Change message format via mmustache!

--- public/js/index.js
+++ public/js/index.js
@@ -14,24 +14,43 @@ socket.on('disconnect', function() {
 
 socket.on('newMessage', function(m) {
   var formattedTime = moment(m.createdAt).format('h:mm a');
-  console.log('newMessage', m );
-  var li = jQuery('<li></li>');
-  li.text(`${m.from} ${formattedTime}: ${m.text}`);
+  var template = jQuery('#message-template').html();
+  var html = Mustache.render(template, {
+    text: m.text,
+    from: m.from,
+    createdAt: formattedTime
+  });
 
-  jQuery('#messages').append(li);
+  jQuery('#messages').append(html);
+
+
+  // var formattedTime = moment(m.createdAt).format('h:mm a');
+  // console.log('newMessage', m );
+  // var li = jQuery('<li></li>');
+  // li.text(`${m.from} ${formattedTime}: ${m.text}`);
+  //
+  // jQuery('#messages').append(li);
 });
 
 socket.on('newLocationMessage', function(message){
   var formattedTime = moment(message.createdAt).format('h:mm a');
-  console.log('newMessage', m );
-
-  var li = jQuery('<li></li>');
-  var a = jQuery('<a target="_blank">My current location</a>');
-
-  li.text(`${message.from}: ${formattedTime}`);
-  a.attr('href', message.url);
-  li.append(a);
-  jQuery('#messages').append(li);
+  var template = jQuery('#location-message-template').html();
+  var html = Mustache.render(template, {
+    from: message.from,
+    createdAt: formattedTime,
+    url: message.url
+  });
+  jQuery('#messages').append(html);
+  // var formattedTime = moment(message.createdAt).format('h:mm a');
+  // console.log('newMessage', m );
+  //
+  // var li = jQuery('<li></li>');
+  // var a = jQuery('<a target="_blank">My current location</a>');
+  //
+  // li.text(`${message.from}: ${formattedTime}`);
+  // a.attr('href', message.url);
+  // li.append(a);
+  // jQuery('#messages').append(li);
 });
 // socket.emit('createMessage', {
 //   from: 'CCC',
